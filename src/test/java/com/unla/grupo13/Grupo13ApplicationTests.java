29d564c8aa0d2c36ff03649380ae0473256ac708
Medina94 <cristian.medina94@gmail.com>
2024-05-26 21:20:06 -0300

    primer commit

---
+++ src/test/java/com/unla/grupo13/Grupo13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
